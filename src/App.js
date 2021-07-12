c98e8a4c993c6e4fbb9500d7eabdc3a628f72285
Gagah Kharismanuary <gmanuary@gmail.com>
2021-07-12 17:03:56 +0700

    update precommit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/react-in-jsx-scope */
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,6 +19,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          role="button"
         >
           Learn React
         </a>
