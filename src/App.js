1abf7ed5ed147438d4e0ae7512985b946eb2b62d
Gagah Kharismanuary <gmanuary@gmail.com>
2021-10-08 13:56:01 +0700

    create embed login ajaib

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope */
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-          dia
-        </p>
+        <iframe type="" src="https://login.ajaib.co.id" title="kyc-bca" width="640" height="640" />
         <a
           className="App-link"
           href="https://reactjs.org"
